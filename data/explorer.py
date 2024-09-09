1ab960ee203f5bd1197c35590e8bfac9c6513123
vicoduor <victoroduorr@gmail.com>
2024-09-10 00:34:38 +0300

    .

--- data/explorer.py
+++ data/explorer.py
@@ -1,5 +1,6 @@
-from .init import curs
+from .init import (curs,conn,IntegrityError)
 from model.explorer import Explorer
+from errors import Missing,Duplicate
 
 curs.execute(
     """create table if not exists explorer(name text primary key,
@@ -7,7 +8,7 @@ curs.execute(
 """)
 
 def row_to_model(row: tuple) ->Explorer:
-    return Explorer(name=[0],country=row[1], description=row[2])
+    return Explorer(name=row[0],country=row[1], description=row[2])
 
 def model_to_dict(explorer:Explorer) -> dict:
     return explorer.dump_model() if explorer else None
@@ -16,7 +17,11 @@ def get_one(name: str) -> Explorer:
     qry = "select * from explorer where name=:name"
     params = {"name": name}
     curs.execute(qry, params)
-    return row_to_model(curs.fetchone())
+    row = curs.fetchone()
+    if row:
+        return row_to_model(row)
+    else:
+        raise Missing(msg=f"Explorer{name} not found")
 
 def get_all() -> list[Explorer]:
     qry = "select * from explorer"
@@ -24,13 +29,20 @@ def get_all() -> list[Explorer]:
     return [row_to_model(row) for row in curs.fetchall()]
 
 def create(explorer: Explorer) -> Explorer:
+    if not explorer: 
+        return None
     qry = """insert into explorer (name, country, description)
-    values (:name, :country, :description)"""
+            values (:name, :country, :description)"""
     params = model_to_dict(explorer)
-    _ = curs.execute(qry, params)
+    try:
+        _ = curs.execute(qry, params)
+    except IntegrityError:
+        raise Duplicate(msg= f"Explorer {explorer.name} alreday exists")
     return get_one(explorer.name)
 
 def modify(name: str, explorer: Explorer) -> Explorer:
+    if not (name and explorer): 
+        return None
     qry = """update explorer
             set country=:country,
             name=:name,
@@ -38,12 +50,17 @@ def modify(name: str, explorer: Explorer) -> Explorer:
             where name=:name_orig"""
     params = model_to_dict(explorer)
     params["name_orig"] = explorer.name
-    _ = curs.execute(qry, params)
-    explorer2 = get_one(explorer.name)
-    return explorer2
+    curs.execute(qry, params)
+    if curs.rowcount == 1:
+        return get_one(explorer.name)
+    else:
+        raise Missing(msg=f"Explorer [name] not found")
 
-def delete(explorer: Explorer) -> bool:
+def delete(name: str):
+    if not name:
+        return False
     qry = "delete from explorer where name = :name"
-    params = {"name": explorer.name}
-    res = curs.execute(qry, params)
-    return bool(res)
+    params = {"name":name}
+    curs.execute(qry, params)
+    if curs.rowcount != 1:
+        raise Missing(msg=f"Explorer {name} not found")
--- web/explorer.py
+++ web/explorer.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException
 from model.explorer import Explorer
-import fake.explorer as service
+from service import explorer as service
+from errors import Duplicate, Missing
 
 router=APIRouter(prefix="/explorer")
 
@@ -14,21 +15,34 @@ def get_all() ->list[Explorer]:
     return service.get_all()
 
 @router.get("/{name}")
-def get_one(name) -> Explorer|None:
-    return service.get_one(name)
+def get_one(name) -> Explorer:
+    try:
+        return service.get_one(name)
+    except Missing as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
 
-@router.post("/")
+@router.post("", status_code=201)
+@router.post("/", status_code=201)
 def create(explorer: Explorer) ->Explorer:
-    return service.create(explorer)
+    try:
+        return service.create(explorer)
+    except Duplicate as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
 
 @router.patch("/")
-def modify(explorer: Explorer) ->Explorer:
-    return service.modify(explorer)
+def modify(name: str ,explorer: Explorer) -> Explorer:
+    try:
+        return service.modify(name, explorer)
+    except Missing as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
 
 @router.put("/")
 def replace(explorer: Explorer) ->Explorer:
     return service.replace(explorer)
 
-@router.delete("/{name}")
+@router.delete("/{name}", status_code=204)
 def delete(name:str):
-    return service.delete(name)
+    try:
+        return service.delete(name)
+    except Missing as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
