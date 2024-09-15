410412b08cee5e5a631c0fa53a67084b4706eed6
vicoduor <victoroduorr@gmail.com>
2024-09-15 06:56:44 +0300

    .

---
+++ fake/user.py
@@ -0,0 +1,49 @@
+from model.user import User
+from errors import Missing,Duplicate
+
+# (no hashed passwords checking in this module)
+
+fakes = [
+    User(name="kwijobo",
+         hash="abc"),
+    User(name="ermagerd",
+         hash='xyz')
+]
+
+def find(name: str) -> User|None:
+    for e in fakes:
+        if e.name == name:
+            return e
+    return None
+
+def check_missing(name: str):
+    if not find(name):
+        raise Missing(msg=f"Missing user {name}")
+    
+def check_duplicate(name: str):
+    if find(name):
+        raise Duplicate(msg=f"Duplicate user {name}")
+
+def get_all() -> list[User]:
+    #return all users
+    return fakes
+
+def get_one(name:str) -> User:
+    #return one user
+    check_missing(name)
+    return find(name)
+
+def create(user: User) -> User:
+    #Add a user
+    check_duplicate(user.name)
+    return user
+
+def modify(name:str, user:User) -> User:
+    #Partially modify a user
+    check_missing(name)
+    return user
+
+def delete(name: str) -> None:
+    #Delete a user
+    check_missing(name)
+    return None
