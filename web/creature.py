3422c996f62ee97a5e55cee2b39209c6c8616e76
vicoduor <victoroduorr@gmail.com>
2024-09-24 13:25:25 +0300

    .

--- web/creature.py
+++ web/creature.py
@@ -1,6 +1,12 @@
-from fastapi import APIRouter
+import os
+from fastapi import APIRouter,HTTPException
 from model.creature import Creature
-import fake.creature as service
+
+if os.getenv("CRYPTID_UNIT_TEST"):
+    from fake import creature as service
+else:
+    from service import creature as service
+from errors import Missing,Duplicate
 
 router = APIRouter(prefix="/creature")
 
@@ -10,15 +16,26 @@ def get_all() -> list[Creature]:
 
 @router.get("/{name}")
 def get_one(name) -> Creature:
-    return service.get_one(name)
+    try:
+        return service.get_one(name)
+    except Missing as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
 
-@router.post("/")
+@router.post("/", status_code=201)
 def create(creature: Creature) -> Creature:
-    return service.create(creature)
+    try:
+        return service.create(creature)
+    except Duplicate as exc:
+        raise HTTPException(status_code=409, detail=exc.msg)
+    
 
 @router.patch("/")
-def modify(creature: Creature) -> Creature:
-    return service.modify(creature)
+def modify(name: str,creature: Creature) -> Creature:
+    try:
+        return service.modify(name,creature)
+    except Missing as exc:
+        raise HTTPException(status_code=404,detail=exc.msg)
+
 
 @router.put("/")
 def replace(creature: Creature) -> Creature:
@@ -26,4 +43,7 @@ def replace(creature: Creature) -> Creature:
 
 @router.delete("/{name}")
 def delete(name: str):
-    return service.delete(name)
+    try:
+        return service.delete(name)
+    except Missing as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
